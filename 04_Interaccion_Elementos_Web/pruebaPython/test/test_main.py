bc4b6c51236a92e78f768197e8e4a451b56fd826
DuberGarcia <duber.garcia@solucionessyh.com>
2025-08-04 10:45:34 -0500

    fix: arreglo del test

--- 04_Interaccion_Elementos_Web/pruebaPython/test/test_main.py
+++ 04_Interaccion_Elementos_Web/pruebaPython/test/test_main.py
@@ -6,7 +6,7 @@ from playwright.async_api import async_playwright,expect
 async def test_run_activo():
     mensaje_alerta = ""
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False,slow_mo=500)
+        browser = await p.chromium.launch(headless=False,slow_mo=600)
         context = await browser.new_context(            
             record_video_dir="videos/",
             record_video_size={"width": 1920, "height": 950},
@@ -14,7 +14,7 @@ async def test_run_activo():
             device_scale_factor=1.0,
         )
         page = await context.new_page()
-        await page.set_viewport_size({"width": 1962, "height": 1080})
+        await page.set_viewport_size({"width": 1962, "height": 950})
         async def manejar_alerta(dialog):
             nonlocal mensaje_alerta
             mensaje_alerta = dialog.message
@@ -27,11 +27,17 @@ async def test_run_activo():
         await page.select_option("#genero","masculino")
         await page.check("#activo")
         await page.click("#Mostrar")
-        await page.screenshot(path="capturas/captura_test.png", full_page=True)
-        await expect(page.locator("#listaUsuarios").locator("#lblnombre")).to_be_visible()
-        await expect(page.locator("#listaUsuarios").locator("#lbledad")).to_be_visible()
-        await expect(page.locator("#listaUsuarios").locator("#lblgenero")).to_be_visible()
-        await expect(page.locator("#listaUsuarios").locator("#lblestado")).to_be_visible()
+        await page.screenshot(path="capturas/captura_test1.png", full_page=True)
+        await page.fill("#nombre", "karmen")
+        await page.fill("#apellido", "Lopez")
+        await page.fill("input.edad", "45")
+        await page.select_option("#genero","femenino")
+        await page.check("#activo")
+        await page.click("#Mostrar")
+        await page.screenshot(path="capturas/captura_test2.png", full_page=True)
+
+        await expect(page.locator("#lblnombre_0")).to_have_text("Nombre: duber garcia ")
+        await expect(page.locator("#lblnombre_1")).to_have_text("Nombre: karmen Lopez ")
         assert "Test" in await page.title()
         assert "duber garcia" in mensaje_alerta
         await context.close()
