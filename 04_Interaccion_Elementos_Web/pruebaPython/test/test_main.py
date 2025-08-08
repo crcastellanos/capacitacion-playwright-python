df52aba8a2d6db191fbf8eb2e85a4e83df8b1863
DuberGarcia <duber.garcia@solucionessyh.com>
2025-08-08 11:04:12 -0500

    feat: cambios en el diseno de las pruebas

--- 04_Interaccion_Elementos_Web/pruebaPython/test/test_main.py
+++ 04_Interaccion_Elementos_Web/pruebaPython/test/test_main.py
@@ -15,29 +15,34 @@ async def test_run_activo():
         )
         page = await context.new_page()
         await page.set_viewport_size({"width": 1962, "height": 950})
+
         async def manejar_alerta(dialog):
             nonlocal mensaje_alerta
             mensaje_alerta = dialog.message
             await dialog.accept()
         page.once("dialog", manejar_alerta)
+
         await page.goto("http://127.0.0.1:5502/formularioTest/index.html")
+
         await page.fill("#nombre", "duber")
-        await page.fill("#apellido", "garcia")
-        await page.fill("input.edad", "22")
-        await page.select_option("#genero","masculino")
-        await page.check("#activo")
-        await page.click("#Mostrar")
+        await page.fill('[data-testId="txtApellido"]', "garcia")
+        await page.fill('input[placeholder="Edad del usuario"]', "22")
+        await page.select_option(".genero","masculino")
+        await page.check("input#activo")
+        await page.click("text=Mostrar")
         await page.screenshot(path="capturas/captura_test1.png", full_page=True)
-        await page.fill("#nombre", "karmen")
-        await page.fill("#apellido", "Lopez")
-        await page.fill("input.edad", "45")
-        await page.select_option("#genero","femenino")
-        await page.check("#activo")
-        await page.click("#Mostrar")
+        
+        await page.locator("#nombre").fill("Jhon")
+        await page.locator('[data-testId="txtApellido"]').fill("Doe")
+        await page.locator('input[placeholder="Edad del usuario"]').fill("45")
+        await page.locator("#genero").select_option("femenino")
+        await page.locator("input#activo").check()
+        await page.locator("text=Mostrar").click()
         await page.screenshot(path="capturas/captura_test2.png", full_page=True)
 
-        await expect(page.locator("#lblnombre_0")).to_have_text("Nombre: duber garcia ")
-        await expect(page.locator("#lblnombre_1")).to_have_text("Nombre: karmen Lopez ")
+
+        await expect(page.locator("h1")).to_have_text("Formulario de Usuario")
+        await expect(page.locator("#nombre")).to_be_empty()
         assert "Test" in await page.title()
         assert "duber garcia" in mensaje_alerta
         await context.close()
